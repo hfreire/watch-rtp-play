44adcb95701c3477a0a9fe316fa38f60b0980eba
Hugo Freire <hfreire@exec.sh>
2017-05-20 17:23:01 +0200

    feat: add support for tv and radio channels

--- README.md
+++ README.md
@@ -1,13 +1,16 @@
-#
+# :tv: Watch and :radio: listen 🇵🇹 RTP Play without a :computer: browser
 
 [![JavaScript Style Guide](https://img.shields.io/badge/code%20style-standard-brightgreen.svg)](http://standardjs.com/)
 [![Build Status](https://travis-ci.org/hfreire/watch-rtp-play.svg?branch=master)](https://travis-ci.org/hfreire/watch-rtp-play)
 [![Coverage Status](https://coveralls.io/repos/github/hfreire/watch-rtp-play/badge.svg?branch=master)](https://coveralls.io/github/hfreire/watch-rtp-play?branch=master)
-[![Greenkeeper badge](https://badges.greenkeeper.io/hfreire/watch-rtp-play.svg)](https://greenkeeper.io/)
 [![](https://img.shields.io/github/release/hfreire/watch-rtp-play.svg)](https://github.com/hfreire/watch-rtp-play/releases)
 [![](https://img.shields.io/badge/license-MIT-blue.svg)](LICENSE)
 
+Watch all 8 TV and 14 radio channels on any device that can play [HTTP Live Streaming](https://en.wikipedia.org/wiki/HTTP_Live_Streaming).
+
 ### Features
+* 8 TV channels :tv: 
+* 14 radio channels :radio:
 * Launch :rocket: inside a Docker container :whale: so you don't need to manage the dependencies :raised_hands: :white_check_mark:
 * Quickly deploy :runner: and easily scale :two_men_holding_hands: the number of servers by using Rancher :white_check_mark:
 
@@ -17,6 +20,11 @@
 ```
 docker run hfreire/watch-rtp-play
 ```
+
+```
+ffplay "http://localhost:3000/playlist.m3u?channel=rtp1"
+```
+
 #### Available environment variables
 Variable | Description | Required | Default value
 :---:|:---:|:---:|:---:
---
+++ src/routes/playlist.js
@@ -0,0 +1,81 @@
+/*
+ * Copyright (c) 2017, Hugo Freire <hugo@exec.sh>.
+ *
+ * This source code is licensed under the license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+const { Route } = require('serverful')
+
+const Joi = require('joi')
+const Boom = require('boom')
+
+const Promise = require('bluebird')
+
+const Logger = require('modern-logger')
+
+const RandomUserAgent = require('random-http-useragent')
+
+const headers = { 'User-Agent': RandomUserAgent.get() }
+
+const channels = require('../channels.json')
+
+const { getAsync } = Promise.promisifyAll(require('request').defaults({ headers }))
+
+class Playlist extends Route {
+  constructor () {
+    super('GET', '/playlist.m3u', 'Playlist', 'Returns a playlist')
+  }
+
+  handler ({ query, info }, reply) {
+    const { channel } = query
+
+    if (!channels[ channel ]) {
+      reply(Boom.badRequest())
+
+      return
+    }
+
+    const { host } = info
+
+    const baseUrl = `http://${host}`
+    let url
+    if (channels[ channel ].is_tv) {
+      url = `https://streaming-live.rtp.pt/liverepeater/smil:${channel}.smil/playlist.m3u`
+    } else {
+      url = `http://streaming-live.rtp.pt/liveradio/${channels[ channel ].name}/playlist.m3u8?DVR`
+    }
+
+    const headers = { 'Referer': `http://www.rtp.pt/play/direto/${channel}` }
+
+    getAsync({ url, headers })
+      .then(({ body }) => {
+        body = body.replace(/chunklist_b640000_slpt.m3u8/, `${baseUrl}/chunklist.m3u8?channel=${channel}&bandwidth=640000`)
+        body = body.replace(/chunklist_b340000_slpt.m3u8/, `${baseUrl}/chunklist.m3u8?channel=${channel}&bandwidth=340000`)
+        body = body.replace(/chunklist_DVR.m3u8/, `${baseUrl}/chunklist.m3u8?channel=${channel}`)
+
+        reply(null, body)
+      })
+      .catch((error) => {
+        Logger.error(error)
+
+        reply(Boom.badImplementation(error.message, error))
+      })
+  }
+
+  auth () {
+    return false
+  }
+
+  validate () {
+    return {
+      query: {
+        channel: Joi.string()
+          .required()
+          .description('the channel')
+      }
+    }
+  }
+}
+
+module.exports = new Playlist()
