c7f5e4e27d169ba43ab90798fbb019ba61b868e1
Hugo Freire <hfreire@exec.sh>
2017-10-19 13:11:29 +0200

    test: improve rtp play request test

--- test/rtp-play-request.js
+++ test/rtp-play-request.js
@@ -5,31 +5,56 @@
  * LICENSE file in the root directory of this source tree.
  */
 
-const Request = require('request-on-steroids')
-
 describe('RTP Play Request', () => {
   let subject
   let Health
+  let RequestOnSteroids
 
   before(() => {
     Health = td.object([ 'addCheck' ])
+
+    RequestOnSteroids = td.constructor([])
   })
 
   afterEach(() => td.reset())
 
-  describe('when constructing', () => {
+  describe('when exporting', () => {
+    beforeEach(() => {
+      td.replace('request-on-steroids', RequestOnSteroids)
+
+      subject = require('../src/rtp-play-request')
+    })
+
+    it('should be instance of request-on-steroids', () => {
+      subject.should.be.instanceOf(RequestOnSteroids)
+    })
+  })
+
+  describe('when exporting and loading request-on-steroids', () => {
     beforeEach(() => {
       td.replace('health-checkup', Health)
 
       subject = require('../src/rtp-play-request')
     })
 
-    it('should add rtp-play health check', () => {
-      td.verify(Health.addCheck('rtp-play', td.matchers.isA(Function)), { times: 1 })
+    it('should create a request-on-steroids with post function', () => {
+      subject.should.have.property('post')
+      subject.post.should.be.instanceOf(Function)
+    })
+  })
+
+  describe('when constructing', () => {
+    beforeEach(() => {
+      td.replace('request-on-steroids', RequestOnSteroids)
+
+      td.replace('health-checkup', Health)
+
+      subject = require('../src/rtp-play-request')
     })
 
-    it('should return an instance of request-on-steroids', () => {
-      subject.should.be.instanceOf(Request)
+    it('should add rtp play health check', () => {
+      td.verify(Health.addCheck('rtp-play', td.matchers.isA(Function)), { times: 1 })
     })
   })
 })
+
