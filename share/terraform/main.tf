8bf1ce45004bef6715965d54dd13afad01c404df
Hugo Freire <hfreire@exec.sh>
2018-06-12 11:47:59 +0200

    chore(terraform): minor tweaks

--- share/terraform/main.tf
+++ share/terraform/main.tf
@@ -2,15 +2,24 @@ provider "aws" {
   region = "${var.aws_region}"
 }
 
+data "template_file" "container_definitions" {
+  template = "${file("${path.module}/container-definitions.json")}"
+
+  vars {
+    name  = "${var.name}"
+    image = "${var.docker_repo}/${var.name}:${var.version}"
+  }
+}
+
 module "watch-rtp-play" {
   //source                     = "github.com/antifragile-systems/antifragile-service"
-  source                     = "../../../antifragile-service"
+  source                = "../../../antifragile-service"
 
-  name                       = "${var.name}"
-  domain_name                = "${var.domain_name}"
-  container_definitions_file = "${path.module}/container-definitions.json"
-  api_quota_limit            = 1000
-  api_quota_offset           = 0
-  api_quota_period           = "DAY"
-  aws_region                 = "${var.aws_region}"
+  name                  = "${var.name}"
+  domain_name           = "${var.domain_name}"
+  container_definitions = "${data.template_file.container_definitions.rendered}"
+  api_quota_limit       = 1000
+  api_quota_offset      = 0
+  api_quota_period      = "DAY"
+  aws_region            = "${var.aws_region}"
 }
--- share/terraform/variables.tf
+++ share/terraform/variables.tf
@@ -2,12 +2,23 @@ variable "infrastructure_name" {
   default = "antifragile-infrastructure"
 }
 
+variable "docker_repo" {}
+
 variable "name" {
   default = "watch-rtp-play"
+}
+
+variable "version" {
+  default = "latest"
 }
 
 variable "domain_name" {}
 
 variable "aws_region" {
   default = "eu-west-1"
+}
+
+variable "api_keys" {
+  type    = "list"
+  default = [ ]
 }
